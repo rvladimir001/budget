e5505b7e34e678ac5f514850a713eadaa2ef20ea
Vladimir <rvladimir001@gmail.com>
2022-01-07 19:42:13 +0300

    добавлен шаблон страницы статистики

---
+++ src/pages/Statistics.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>Pie Chart</h2>
+  <div style="max-width: 400px">
+    <vue3-chart-js v-bind="{ ...doughnutChart }" />
+  </div>
+</template>
+
+<script>
+import Vue3ChartJs from "@j-t-mcc/vue3-chartjs";
+
+export default {
+  name: "Statistics",
+  components: {
+    Vue3ChartJs,
+  },
+  setup() {
+    const doughnutChart = {
+      type: "doughnut",
+      data: {
+        labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
+        datasets: [
+          {
+            backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
+            data: [40, 20, 80, 10],
+          },
+        ],
+      },
+    };
+
+    return {
+      doughnutChart,
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
